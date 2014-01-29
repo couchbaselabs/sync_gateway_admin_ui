82f75183830a6883feea572d8cec2d862c50f082
Chris Anderson <jchris@couchbase.com>
2014-01-29 11:29:59 -0800

    clean on build

--- bundle.sh
+++ bundle.sh
@@ -1,2 +1,4 @@
 #/bin/sh
-mkdir -p src/github.com/couchbaselabs/sync_gateway_admin_ui/ && cat assets/index.html | go-bindata -func Admin_bundle_html -pkg sync_gateway_admin_ui | gofmt > src/github.com/couchbaselabs/sync_gateway_admin_ui/admin_bundle.go
+rm -rf pkg/
+mkdir -p src/github.com/couchbaselabs/sync_gateway_admin_ui/
+cat assets/index.html | go-bindata -func Admin_bundle_html -pkg sync_gateway_admin_ui | gofmt > src/github.com/couchbaselabs/sync_gateway_admin_ui/admin_bundle.go
