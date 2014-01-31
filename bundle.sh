0bfbef16953c4d3b4fe268bd0411244d4017e42e
Chris Anderson <jchris@couchbase.com>
2014-01-30 17:29:09 -0800

    update to live in vendor

--- bundle.sh
+++ bundle.sh
@@ -1,4 +1,2 @@
 #/bin/sh
-rm -rf pkg/
-mkdir -p src/github.com/couchbaselabs/sync_gateway_admin_ui/
-cat assets/index.html | go-bindata -func Admin_bundle_html -pkg sync_gateway_admin_ui | gofmt > src/github.com/couchbaselabs/sync_gateway_admin_ui/admin_bundle.go
+cat assets/index.html | go-bindata -func Admin_bundle_html -pkg sync_gateway_admin_ui | gofmt > admin_bundle.go
