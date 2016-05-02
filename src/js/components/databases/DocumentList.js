ca58e52604726d18609a7b1b33499eacca7a9023
Pasin Suriyentrakorn <pasin@couchbase.com>
2016-05-02 16:16:58 -0700

    Implement document deletion
    Implemented multiple document selection and deletion.

--- src/js/components/databases/DocumentList.js
+++ src/js/components/databases/DocumentList.js
@@ -1,8 +1,8 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { makePath } from '../../utils';
-import { fetchDocs } from '../../api';
-import { Button, Table } from 'react-bootstrap';
+import { fetchDocs, deleteDoc } from '../../api';
+import { Button, Table, Checkbox } from 'react-bootstrap';
 import { Box, BoxHeader, BoxTools, BoxBody, BoxFooter, 
   Icon, Space } from '../ui';
 
@@ -12,6 +12,9 @@ class DocumentList extends React.Component {
  
     this.previousOnClick = this.previousOnClick.bind(this);
     this.nextOnClick = this.nextOnClick.bind(this);
+    this.allRowsCheckedOnChange = this.allRowsCheckedOnChange.bind(this);
+    this.singleRowCheckedOnChange = this.singleRowCheckedOnChange.bind(this);
+    this.deleteOnClick = this.deleteOnClick.bind(this);
 
     this.state = {
       pageSize: 10,
@@ -19,7 +22,10 @@ class DocumentList extends React.Component {
       curPage: 0,
       startKeys: [ ],
       isFetching: false,
-      error: undefined
+      isDeleting: false,
+      error: undefined,
+      allRowsChecked: { },  // { <page-index>: <true|undefined> }
+      selectedRows: { }     // { <page-index>: { <row.id> : <row.value.rev> } }
     }
   }
 
@@ -53,26 +59,30 @@ class DocumentList extends React.Component {
     const {pageSize, startKeys } = this.state;
     let displayRows = rows;
     let newStartKeys = [ ];
-    if (displayRows && displayRows.length > 0) {
-      if (page === 0)
-        newStartKeys = [ undefined ];
-      else {
+    if (displayRows) {
+      if (page === 0) {
+        if (displayRows.length > 0)
+          newStartKeys = [ undefined ];
+      } else {
         newStartKeys = startKeys.slice(0, page);
-        newStartKeys.push(displayRows[0].id);
+        if (displayRows.length > 0)
+          newStartKeys.push(displayRows[0].id);
       }
-
+      
       if (displayRows.length > pageSize) {
         newStartKeys.push(displayRows[pageSize].id);
         displayRows = displayRows.slice(0, -1);
       }
     }
-
+    
     this.setState(state => {
       return Object.assign({ }, state, { 
         rows: displayRows, 
         curPage: page, 
         startKeys: newStartKeys });
     });
+    
+    validateSelectedRows(page, displayRows);
   }
 
   previousOnClick() {
@@ -91,6 +101,138 @@ class DocumentList extends React.Component {
   hasPrevious() {
     const { curPage, rows } = this.state;
     return (rows && curPage > 0);
+  }
+  
+  validateSelectedRows(curPage, rows) {
+    let { selectedRows, allRowsChecked } = this.state;
+    let selectedMap = selectedRows[curPage];
+     
+    const curSelectedRowIds = Object.getOwnPropertyNames(selectedMap);
+    if (curSelectedRowIds.length > 0) {
+      const rowIds = new Set();
+      rows.forEach(row => {
+        rowIds.add(row.id);
+      });
+      
+      let newSelectedMap;
+      
+      // Remove the selected rows that do not exist on the page anymore:
+      curSelectedRowIds.forEach(selectedRowId => {
+        if (!rowIds.has(selectedRowId)) {
+          if (!newSelectedMap)
+            newSelectedMap = Object.assign({ }, selectedMap);
+          newSelectedMap[selectedRowId] = undefined;
+        }
+      });
+      
+      if (newSelectedMap) {
+        selectedRows = Object.assign({ }, selectedRows, { 
+          [curPage]: newSelectedMap 
+        });
+        
+        // Reset all-row-checked status:
+        if (allRowsChecked[curPage] !== undefined) {
+          allRowsChecked = Object.assign({ }, allRowsChecked, { 
+            [curPage]: undefined 
+          })
+        }
+        
+        this.setState(state => {
+          return Object.assign({ }, state, { selectedRows, allRowsChecked } );
+        }); 
+      }
+    }
+  }
+  
+  allRowsCheckedOnChange(event) {
+    this.updateAllRowsChecked(event.target.checked);
+  }
+  
+  updateAllRowsChecked(checked) {
+    let { curPage, rows, allRowsChecked, selectedRows } = this.state;
+    
+    if (checked) {
+      const selectedMap = { };
+      for (const row of rows) {
+        selectedMap[row.id] = row.value.rev;
+      }
+      
+      selectedRows = Object.assign({ }, selectedRows, { 
+        [curPage]: selectedMap 
+      });
+    } else {  
+      selectedRows = Object.assign({ }, selectedRows, { 
+        [curPage]: undefined 
+      });
+    }
+    
+    allRowsChecked = Object.assign({ }, allRowsChecked, {
+      [curPage]: (event.target.checked ? true : undefined)
+    });
+    
+    this.setState(state => {
+      return Object.assign({ }, this.state, { allRowsChecked, selectedRows });
+    });
+  }
+  
+  singleRowCheckedOnChange(event) {
+    let { curPage, rows, selectedRows } = this.state;
+    
+    const row = rows[event.target.value]
+    let selectedMap = Object.assign({ }, selectedRows[curPage], {
+      [row.id]: (event.target.checked ? row.value.rev : undefined)
+    });
+    
+    selectedRows = Object.assign({ }, selectedRows, { 
+      [curPage]: selectedMap  
+    });
+    
+    this.setState(state => {
+      return Object.assign({ }, this.state, { selectedRows });
+    });
+  }
+  
+  isAllRowsChecked() { 
+    let { curPage, allRowsChecked } = this.state;
+    return allRowsChecked[curPage] !== undefined;
+  }
+  
+  isRowChecked(rowId) {
+    let { curPage, selectedRows } = this.state;
+    const selectedMap = selectedRows[curPage];
+    return selectedMap && selectedMap[rowId] !== undefined;
+  }
+  
+  hasSelectedRows() {
+    const { curPage, selectedRows } = this.state;
+    const selectedMap = selectedRows[curPage];
+    return selectedMap && Object.getOwnPropertyNames(selectedMap).length > 0;
+  }
+  
+  setDeleteStatus(isDeleting, error) {
+    this.setState(state => {
+      return Object.assign({ }, state, { isDeleting, error });
+    });
+  }
+  
+  deleteOnClick() {
+    const { db } = this.props.params;
+    const { curPage, selectedRows } = this.state;
+    const selectedMap = selectedRows[curPage];
+    
+    let deleteDocs = Object.keys(selectedMap).map(docId => {
+      const revId = selectedMap[docId];
+      return deleteDoc(db, docId, revId);
+    });
+    
+    this.setDeleteStatus(true);
+    Promise.all(deleteDocs)
+      .then(results => {
+        this.setDeleteStatus(false);
+        this.fetchDocuments(0);
+      }).catch(error => {
+        this.setDeleteStatus(false, error);
+      });
   }
 
   render() {
@@ -124,9 +266,8 @@ class DocumentList extends React.Component {
         <Link to={makePath('databases', db, 'documents', 'new')}>
           <Button bsSize="sm"><Icon name="plus"/></Button>
         </Link><Space/>
-        <Link to={makePath('databases', db, 'documents', 'new')}>
-          <Button bsSize="sm"><Icon name="trash-o"/></Button>
-        </Link>
+        <Button bsSize="sm" disabled={!this.hasSelectedRows()} 
+          onClick={this.deleteOnClick}><Icon name="trash-o"/></Button>
         <div className="pull-right">
           {paginationLabel}<Space/>
           <div className="btn-group">
@@ -143,15 +284,21 @@ class DocumentList extends React.Component {
 
     const tableHeader = (
       <tr>
-        <th><input type="checkbox"/></th>
+        <th>
+          <input type="checkbox" checked={this.isAllRowsChecked()} 
+            onChange={this.allRowsCheckedOnChange}/>
+        </th>
         <th>ID</th>
         <th>Rev</th>
       </tr>
     );
 
-    const tableRows = rows.map(row =>
+    const tableRows = rows.map((row, index) =>
       <tr key={row.id}>
-        <td><input type="checkbox"/></td>
+        <td>
+          <input type="checkbox" checked={this.isRowChecked(row.id)} 
+            value={index} onChange={this.singleRowCheckedOnChange}/>
+        </td>
         <td>
           <Link to={makePath('databases', db, 'documents', row.id)}>
             {row.id}
