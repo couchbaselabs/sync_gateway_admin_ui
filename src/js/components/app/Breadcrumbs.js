0e99af5224db018afed5750c25edf1f6e8dfe0d1
Pasin Suriyentrakorn <pasin@couchbase.com>
2016-05-05 10:18:21 -0700

    Add ignore paths to the Breadcrumbs
    Currently ignore '/' path.

--- src/js/components/app/Breadcrumbs.js
+++ src/js/components/app/Breadcrumbs.js
@@ -7,10 +7,12 @@ const Breadcrumbs = (props) => {
     return null;
   
   const routes = props.routes.filter((route) => route.path && true);
-  const breadcrumbs = getAllPaths(routes)
+  const ignorePaths = ['/'];
+  const breadcrumbs = getAllPaths(routes, ignorePaths)
     .map(path => resolveBreads(path, props.params, {'/' : 'Home'}))
     .map((bread, index, me) => {
-      const icon = index == 0 && (<i className="fa fa-home"/>);
+      const icon = index == 0 && 
+        bread.path === '/' && (<i className="fa fa-home"/>);
       return (
         <li key={index}>
           <Link onlyActiveOnIndex={true} activeClassName={null} to={bread.path}>
@@ -26,34 +28,41 @@ const Breadcrumbs = (props) => {
   );
 }
 
-const getAllPaths = (routes) => {
+const getAllPaths = (routes, ignorePaths) => {
   const paths = [];
   for (let i = 0; i < routes.length; i++) {
     const { path } = routes[i];
+    let absPath;
     if (path.startsWith('/'))
-      paths.push(path);
+      absPath = path;
     else {
       if (i == 0)
         throw 'The root path must begin with "/"';
       const parent = paths[i-1];
-      paths.push(parent + (parent.endsWith('/') ? '' : '/') + path);
+      absPath = parent + (parent.endsWith('/') ? '' : '/') + path;
     }
+    
+    if (ignorePaths.indexOf(absPath) != 0)
+      paths.push(absPath);
   }
   return paths;
 }
 
-const resolveBreads = (path, params, custom) => {
-  let comps = path.split('/').filter(c => c.length > 0).map(c => {
+const resolveBreads = (path, params, names) => {
+  let comps = path.split('/').filter(c => {
+    return c.length > 0
+  }).map(c => {
     if (c.startsWith(':'))
       return params[c.substring(1)] || c;
     return c;
   });
+  
   if (comps.length == 0) 
     comps = [''];
 
   const resolvedPath = makePath(...comps);
 
-  let name = custom[path];
+  let name = names[path];
   if (!name)
     name = comps.length > 0 ? comps[comps.length - 1] : null;
   name = name || '?';
